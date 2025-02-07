daed9755c825813d4d764ae090c5bb99ed2de1ef
KaiqueDantas14 <kaiquedantasdev@gmail.com>
2025-02-07 07:17:31 +0000

    Nivel iniciante

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,32 +6,33 @@ int main(){
 
     //Declaração das variaveis
     char Estado[3], CodigoDaCarta[4], NomeDaCidade[50];
-    int PontosTuristicos, area;
-    long long int PIB;
-    float População;
+    int PontosTuristicos, PIB, Populacao, area;
 
     //Entrada de dados
     printf("Insira os dados da carta\n");
     printf("Estado (ex: SP): ");
-    scanf("%2s", Estado);
+    scanf("%s", &Estado);
 
     printf("Codigo da carta (ex: A01): ");
-    scanf("%3s", CodigoDaCarta);
+    scanf("%3s", &CodigoDaCarta);
 
     while (getchar() != '\n'); // limpa o buffer antes do nome da cidade
 
-    printf("Nome da cidade: ");
-    scanf(" %[^\n]", NomeDaCidade); //  '%[^\n]' lê espaços
+    printf("Nome da cidade (Exemplo: Sao Paulo): ");
+    scanf(" %[^\n]", &NomeDaCidade); //  '%[^\n]' lê espaços
 
-    printf("População (em milhões, exemplo: 11.45 milhões): ");
-    scanf("%d", &População);
+    printf("População (em milhões, exemplo: 11451999 milhões): ");
+    scanf(" %d", &Populacao);
+   
+    while (getchar() != '\n');
 
-    printf("Área (em km²): ");
+    printf("Área (em km², exemplo: 1521202 milhões): ");
     scanf(" %d", &area);
 
      while (getchar() != '\n');
 
     printf("PIB (em bilhões, exemplo: 828 bilhões): ");
+    scanf("%d", &PIB);
 
     while (getchar() != '\n'); 
 
@@ -44,9 +45,9 @@ int main(){
     printf("Estado: %s \n", Estado);
     printf("Codigo da carta: %s \n", CodigoDaCarta);
     printf("Nome da Cidade: %s \n", NomeDaCidade);
-    printf("População: %d \n", População);
+    printf("População: %d milhões \n", Populacao);
     printf("Área: %d km² \n", area);
-    printf("PIB: %lld bilhões \n", PIB); // Exibe de volta em bilhões
+    printf("PIB: %d bilhões \n", PIB);
     printf("Numero de pontos turisticos: %d \n", PontosTuristicos);
 
     return 0;
