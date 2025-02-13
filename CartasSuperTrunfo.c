a8f32eee73770dbc1a7a3ec2b390a4bbb0baf9b5
KaiqueDantas14 <kaiquedantasdev@gmail.com>
2025-02-13 22:33:18 +0000

    Nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,13 +2,16 @@
 
 // Nivel Mestre
 
+/*O codigo vai comparar as duas cartas cadastradas pelo usuario e decidir um vencedor baseado em quem tiver a menor densidade populacional,
+ caso haja empate, o vencedor vai ser a carta com maior atributo geral, que é a soma de todos os atributos.S*/
+
 //Estrutura para armazenar os dados da carta
 struct CartasSuperTrunfo
 {
     char Estado[3];
     char CodigoDaCarta[5];
     char NomeDaCidade[60];
-    long long int PIB, Populacao, area;
+    long long int PIB, Populacao, area, atributosGerais;
     float DensidadePopulacional, PIBperCapita;
     int PontosTuristicos;
 };
@@ -22,20 +25,20 @@ struct CartasSuperTrunfo
     scanf("%s", Carta->Estado);
 
     printf("Codigo da carta (ex: A01): ");
-    scanf("%3s", &Carta->CodigoDaCarta);
+    scanf("%3s", Carta->CodigoDaCarta);
 
     while (getchar() != '\n'); // limpa o buffer antes do nome da cidade
 
     printf("Nome da cidade (Exemplo: Sao Paulo): ");
-    scanf(" %[^\n]", Carta->NomeDaCidade); //  '%[^\n]' lê espaços
-
+    fgets(Carta->NomeDaCidade, sizeof(Carta->NomeDaCidade), stdin);
+    
     printf("População (em milhões, exemplo: 11451999): ");
-    scanf(" %d", &Carta->Populacao);
+    scanf(" %lld", &Carta->Populacao);
    
     while (getchar() != '\n');
 
     printf("Área (em km², exemplo: 1521202): ");
-    scanf(" %d", &Carta->area);
+    scanf(" %lld", &Carta->area);
 
      while (getchar() != '\n');
 
@@ -49,7 +52,8 @@ struct CartasSuperTrunfo
 
     // Calculos 
     Carta->DensidadePopulacional = (float) Carta->Populacao / Carta->area;
-    Carta->PIBperCapita = (double) Carta->PIB / Carta->Populacao;
+    Carta->PIBperCapita = (float) Carta->PIB / Carta->Populacao;
+    Carta->atributosGerais = (double) Carta->area + Carta->PIB + Carta->PontosTuristicos + Carta->Populacao;
 
     }
 
@@ -63,11 +67,22 @@ int main(){
     CadastrarCarta(&carta2, 2);
     
     //Saida de dados
-    printf("Carta 1 - Estado: %s | Cidade: %s | PIB: %lld | População: %lld | Área: %lld | Densidade: %.2f | PIB per Capita: %.2f\n",
-           carta1.Estado, carta1.NomeDaCidade, carta1.PIB, carta1.Populacao, carta1.area, carta1.DensidadePopulacional, carta1.PIBperCapita);
+    printf("Carta 1 - Estado: %s | Cidade: %s | PIB: %lld | População: %lld | Área: %lld | Densidade: %.2f | PIB per Capita: %.2f\n | Pontos Turisticos: %d",
+           carta1.Estado, carta1.NomeDaCidade, carta1.PIB, carta1.Populacao, carta1.area, carta1.DensidadePopulacional, carta1.PIBperCapita, carta1.PontosTuristicos);
 
-    printf("Carta 2 - Estado: %s | Cidade: %s | PIB: %lld | População: %lld | Área: %lld | Densidade: %.2f | PIB per Capita: %.2f\n",
-           carta2.Estado, carta2.NomeDaCidade, carta2.PIB, carta2.Populacao, carta2.area, carta2.DensidadePopulacional, carta2.PIBperCapita);
+    printf("Carta 2 - Estado: %s | Cidade: %s | PIB: %lld | População: %lld | Área: %lld | Densidade: %.2f | PIB per Capita: %.2f\n | Pontos Turisticos: %d",
+           carta2.Estado, carta2.NomeDaCidade, carta2.PIB, carta2.Populacao, carta2.area, carta2.DensidadePopulacional, carta2.PIBperCapita, carta2.PontosTuristicos);
 
+           // Comparação das cartas
+     if(carta1.DensidadePopulacional<carta2.DensidadePopulacional){
+        printf("Vencedor: %s", carta1.NomeDaCidade);
+     } else if(carta2.DensidadePopulacional<carta1.DensidadePopulacional){
+        printf("Vencedor: %s", carta2.NomeDaCidade);
+     } else if(carta1.atributosGerais>carta2.atributosGerais){
+        printf("Vencedor: %s",carta1.NomeDaCidade);    
+     } else { 
+    printf("Vencedor: %s", carta2.NomeDaCidade);  
+     }
+        
     return 0;
 }
